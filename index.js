2dc45887f8245c076395fc0bb13b0cb3261946a9
urstrulynishkarsh <206301010@gkv.ac.in>
2023-05-28 17:13:01 +0530

    first commit

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
-const express=require('express')
+const express = require('express')
 const cors = require('cors');
-const app= express()
+const app = express()
 
 app.use(cors());
 app.use(cors({
-  origin: 'https://64732e8d1619082276f2ebab--vocal-nasturtium-88360c.netlify.app/',
+  origin: 'https://cubechamp2.netlify.app/',
 }));
 require("dotenv").config()
 
-const PORT=process.env.PORT||5000
+const PORT = process.env.PORT || 5000
 
 // middleware
 app.use(express.json())
 
-const cubechamp=require('./router/cubechamp')
+const cubechamp = require('./router/cubechamp')
 
 // mount
-app.use("/api/v1",cubechamp)
+app.use("/api/v1", cubechamp)
 
 // connection with db
-const connectWithDb=require('./config/database')
+const connectWithDb = require('./config/database')
 connectWithDb();
 
 app.listen(PORT, () => {
-    console.log(`THE SERVER IS UP AND RUNNING AT PORT ${PORT}`);
-  });
+  console.log(`THE SERVER IS UP AND RUNNING AT PORT ${PORT}`);
+});
 
 //   default routes
- app.get("/", (req, res) => {
-    res.send(`<h1>Backend is Running and this is '/' Route</h1>`);
-  });
-  
+app.get("/", (req, res) => {
+  res.send(`<h1>Backend is Running and this is '/' Route</h1>`);
+});
+
 
